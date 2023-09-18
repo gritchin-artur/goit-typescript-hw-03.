0acffc25d83409bf70abe9f820f38c3602f4ccff
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-09-18 19:51:46 +0200

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,41 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.door = false;
+  }
+
+  public comeIn(tenant: Person) {
+    this.tenants.push(tenant);
+  }
+}
+
+class MyHouse extends House {
+  public openDoor(isOpen: object) {
+    if (isOpen === this.key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +44,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
