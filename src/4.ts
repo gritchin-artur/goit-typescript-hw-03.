ad6a8c2455ea254221cadda073c5972e004a1e13
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-10-04 11:17:05 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -18,19 +18,23 @@ abstract class House {
   protected door: boolean;
   protected key: Key;
   protected tenants: Person[] = [];
+  abstract openDoor(key: Key): void;
 
   constructor(key: Key) {
     this.door = false;
+    this.key = key;
   }
 
   public comeIn(tenant: Person) {
-    this.tenants.push(tenant);
+    if (this.door && this.key === tenant.getKey()) {
+      this.tenants.push(tenant);
+    }
   }
 }
 
 class MyHouse extends House {
-  public openDoor(isOpen: object) {
-    if (isOpen === this.key) {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
       this.door = true;
     }
   }
